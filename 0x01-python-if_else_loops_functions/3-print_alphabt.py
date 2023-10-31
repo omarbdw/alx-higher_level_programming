2c7359dae94d1921d3b474d1c9594af71957257c
omarbdw <omarbdw@yahoo.com>
2023-10-31 13:21:36 -0700

    4

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 101):
-    print("{}".format(chr(i)), end="")
-for i in range(102, 113):
-    print("{}".format(chr(i)), end="")
-for i in range(114, 123):
-    print("{}".format(chr(i)), end="")
+for i in range(97, 123):
+     if i != 101 and i != 113:
+         print("{}".format(chr(i)), end="")
