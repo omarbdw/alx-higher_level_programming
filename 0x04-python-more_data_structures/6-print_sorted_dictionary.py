807b8ae367cfb21fd03b8804066d059743dea091
omarbdw <omarbdw@yahoo.com>
2023-11-08 14:15:19 +0200

    6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sortedDic={}
+    sortedDic = a_dictionary
+    sortedDic = sorted(sortedDic.items())
+    for key, value in sortedDic:
+        print("{} : {}".format(key, value))
