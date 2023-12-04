57f04ca0cffd3292f7a973b84f9d6418f7c13e94
omarbdw <omarbdw@yahoo.com>
2023-12-04 16:03:47 +0200

    0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+def lookup(obj):
+    """
+    This function takes an object as input and returns a list of all
+    the attributes and methods of that object.
+    Args:
+        obj: The object to inspect.
+    Returns:
+        A list of strings representing the attributes
+        and methods of the object.
+    """
+    return dir(obj)
