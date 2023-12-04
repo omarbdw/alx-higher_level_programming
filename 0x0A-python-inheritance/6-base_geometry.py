8a3b76043def4dec74113a759fbc5fd77be24894
omarbdw <omarbdw@yahoo.com>
2023-12-04 21:39:56 +0200

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """An empty class"""
+    def area(self):
+        """Raises an Exception with the message area() is not implemented"""
+        raise Exception("area() is not implemented")
