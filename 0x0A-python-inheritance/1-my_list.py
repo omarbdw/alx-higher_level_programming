4eab83211e79f60c4d6b2dc0a991e35ef0f0e26d
omarbdw <omarbdw@yahoo.com>
2023-12-04 21:05:38 +0200

    1.3

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,10 @@
 
 class MyList(list):
     """This class inherits from list"""
+    def __init__(self):
+        """This method initializes an instance of the class"""
+        super().__init__()
+
     def print_sorted(self):
         """This method prints the list, but sorted (ascending sort)"""
         print(sorted(self))
