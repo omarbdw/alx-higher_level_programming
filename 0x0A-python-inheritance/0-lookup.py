75ca1b6620aa76e739707d7d6773643abc86e37c
omarbdw <omarbdw@yahoo.com>
2023-12-04 16:07:24 +0200

    0.2

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ This module contains a function that returns the list of available"""
+
+
 def lookup(obj):
     """
     This function takes an object as input and returns a list of all
