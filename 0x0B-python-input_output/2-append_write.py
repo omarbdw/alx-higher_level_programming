d63fc91150ec1121b914b5bbfadc8f230979cd34
omarbdw <omarbdw@yahoo.com>
2023-12-05 13:17:34 +0200

    2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module contains a function that appends a string at the end of
+a text file (UTF8) and returns the number of characters added
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    a function that appends a string at the end of a text
+    file (UTF8) and returns the number of characters added
+    """
+    with open(filename, mode='a', encoding='utf-8') as f:
+        return f.write(text)
