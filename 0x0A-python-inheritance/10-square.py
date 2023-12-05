769ea13ed539be8e1ece1f65fce56058997e3295
omarbdw <omarbdw@yahoo.com>
2023-12-05 12:04:55 +0200

    11.2

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -17,3 +17,7 @@ class Square(Rectangle):
     def area(self):
         """ Return area of square """
         return self.__size ** 2
+    
+    def __str__(self):
+        """ Return string representation of square """
+        return "[Square] {}/{}".format(self.__size, self.__size)
