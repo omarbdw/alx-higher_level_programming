e52bf150d6106f4e7dc18e4c14b54a23110e2077
omarbdw <omarbdw@yahoo.com>
2023-12-05 14:26:24 +0200

    7.4

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-""" Module that contains the function load_from_json_file """
+"""
+ save items to a file.
+"""
+
 import sys
 import os.path
+
 args = sys.argv[1:]
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-my_list = []
+item = []
 if os.path.exists("./add_item.json"):
-    my_list = load_from_json_file(filename)
-save_to_json_file((my_list + args), filename)
+    item = load_from_json_file("add_item.json")
+
+save_to_json_file(item + args, "add_item.json")
