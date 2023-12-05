3896aec6b3f89e375ad4ba82127fcc4fa73294ab
omarbdw <omarbdw@yahoo.com>
2023-12-05 12:01:10 +0200

    10.2

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,27 +1,6 @@
 #!/usr/bin/python3
-"""Rectangle class Module"""
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
-
-
-""" class Rectangle that inherits from BaseGeometry """
-
-
-class Rectangle(BaseGeometry):
-    """ class Rectangle that inherits from BaseGeometry """
-    def __init__(self, width, height):
-        """ Instantiation with width and height """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """ Return area of rectangle """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """ Return string representation of rectangle """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+"""Square class Module"""
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 """ class Square that inherits from Rectangle """""
