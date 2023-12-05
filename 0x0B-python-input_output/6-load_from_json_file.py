503460570ef47cdc8d30c04806e8174da0c97145
omarbdw <omarbdw@yahoo.com>
2023-12-05 13:39:52 +0200

    6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module that contains the function load_from_json_file """
+
+
+def load_from_json_file(filename):
+    """ Function that creates an Object from a JSON file """
+    import json
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    return data    
