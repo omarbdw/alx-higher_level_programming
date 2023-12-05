f9a040dee5de7ef0f6cec7d0114fb5bd4ebb7bed
omarbdw <omarbdw@yahoo.com>
2023-12-05 15:41:22 +0200

    8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module that contains the function class_to_json """
+import json
+
+
+def class_to_json(obj):
+    """ Function that returns the dictionary description with
+    simple data structure (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object """
+    return obj.__dict__
