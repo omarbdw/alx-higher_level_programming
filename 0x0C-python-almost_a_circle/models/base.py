4dc364af5534dcc29362b2fb1a60195cd4449ab1
omarbdw <omarbdw@yahoo.com>
2023-12-07 11:05:56 +0200

    15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,3 +13,21 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    def to_json_string(list_dictionaries):
+        """Returns JSON representation of list_dictionaries"""
+        import json
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+    
+    def save_to_file(cls, list_objs):
+        """Writes JSON string representation of list_objs to file"""
+        import json
+        if list_objs is None:
+            list_objs = []
+        list_dicts = []
+        for obj in list_objs:
+            list_dicts.append(obj.to_dictionary())
+        with open(cls.__name__ + ".json", "w") as f:
+            f.write(cls.to_json_string(list_dicts))
