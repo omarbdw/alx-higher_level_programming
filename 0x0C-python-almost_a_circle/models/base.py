1d4dd0cc3d148b696dc7f4695e6c484c220c07a1
omarbdw <omarbdw@yahoo.com>
2023-12-07 10:06:55 +0200

    1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Base module"""
+
+class Base:
+    """Base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialization method"""
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,16 @@
+import unittest
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    def test_init_with_id(self):
+        base = Base(10)
+        self.assertEqual(base.id, 10)
+
+    def test_init_without_id(self):
+        base1 = Base()
+        base2 = Base()
+        self.assertEqual(base1.id, 1)
+        self.assertEqual(base2.id, 2)
+
+if __name__ == '__main__':
+    unittest.main()
