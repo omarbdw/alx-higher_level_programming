3f57f6b638148ea86d757be70256c99fe206e0a4
omarbdw <omarbdw@yahoo.com>
2024-01-16 15:08:07 +0200

    all

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,2 @@
 -- Update the score of Bob to 10
-UPDATE second_table
-SET score = 10
-WHERE name = 'Bob';
-
+UPDATE `second_table` SET `score`=10 WHERE `second_table`.`name`="Bob";
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,6 @@
 -- Create a new database hbtn_0c_0 in your MySQL server
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT
-);
-
-INSERT INTO hbtn_0c_0.second_table (id, name, score)
-VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
