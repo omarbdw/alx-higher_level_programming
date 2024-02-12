79eae1f4c7a6dc4a83a23161cc9ba23c08d18a1b
omarbdw <omarbdw@yahoo.com>
2024-02-12 12:02:58 +0200

    10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
 
-const factorial = (n) => {
-  if (isNaN(n)) {
-    return 1;
+function factorial (n) {
+  if (n < 0) {
+  return (-1);
   }
-
-  if (n === 0) {
-    return 1;
+  if (n === 0 || isNaN(n)) {
+  return (1);
   }
-
-  return n * factorial(n - 1);
-};
+  return (n * factorial(n - 1));
+  }
 
 const num = parseInt(process.argv[2]);
 console.log(factorial(num));
