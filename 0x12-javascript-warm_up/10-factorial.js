902801dcf9a2cc42bc6248e9ca349a877d48b805
omarbdw <omarbdw@yahoo.com>
2024-02-12 12:06:44 +0200

    format

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,13 +2,13 @@
 
 function factorial (n) {
   if (n < 0) {
-  return (-1);
+    return -1;
   }
   if (n === 0 || isNaN(n)) {
-  return (1);
+    return 1;
   }
-  return (n * factorial(n - 1));
-  }
+  return n * factorial(n - 1);
+}
 
 const num = parseInt(process.argv[2]);
 console.log(factorial(num));
