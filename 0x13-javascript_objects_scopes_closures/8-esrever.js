1bcc2bb6ef61e79ce89cf9fb919e0503344c366a
omarbdw <omarbdw@yahoo.com>
2024-02-13 11:27:44 +0200

    all

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  var reversedList = [];
+  for (var i = list.length - 1; i >= 0; i--) {
+    reversedList.push(list[i]);
+  }
+  return reversedList;
+};
