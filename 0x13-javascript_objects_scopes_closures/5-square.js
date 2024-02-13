6ffa8994e18fc69251ac6635ebfdf37b4dc73e6c
omarbdw <omarbdw@yahoo.com>
2024-02-13 12:53:43 +0200

    styleb

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  const reversedList = [];
-  for (let i = list.length - 1; i >= 0; i--) {
-    reversedList.push(list[i]);
+  let length = list.length - 1;
+  let i = 0;
+  while ((length - i) > 0) {
+    const aux = list[length];
+    list[length] = list[i];
+    list[i] = aux;
+    i++;
+    length--;
   }
-  return reversedList;
+  return list;
 };
