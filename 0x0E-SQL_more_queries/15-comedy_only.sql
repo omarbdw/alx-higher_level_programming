593917205fff65be0e38752c64db83dd69e0ebd8
omarbdw <omarbdw@yahoo.com>
2024-02-17 16:45:56 +0200

    installation

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++
@@ -1,7 +0,0 @@
--- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 14-my_genres.sql)
-USE hbtn_0d_tvshows;
-SELECT tv_shows.title
-FROM tv_shows
-JOIN tv_genres ON tv_shows.id = tv_genres.show_id
-WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_shows.title ASC;
--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++
@@ -1,6 +0,0 @@
--- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 15-comedy_only.sql)
-SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL') AS genre
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, genre ASC;
