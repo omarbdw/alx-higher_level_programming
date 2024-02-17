50184cc567b8b21424146c1660515fc50017dd7a
root <root@Omar-PC.localdomain>
2024-02-17 17:51:03 +0200

    0.b

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+""" 0-select_states module"""
 import MySQLdb
 import sys
-if __name__ == "__main__":
+
+def select_states():
+	"""
+	Connects to a MySQL server and selects all rows from the 'states' table.
+	Prints the results.
+	"""
 	# Connect to MySQL server
-	db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+	db = MySQLdb.connect(host="localhost", port=3306,
+						 user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
 	# Create a cursor object
 	cursor = db.cursor()
@@ -21,3 +28,6 @@ if __name__ == "__main__":
 	# Close the cursor and database connection
 	cursor.close()
 	db.close()
+
+if __name__ == "__main__":
+	select_states()
