e39eb84afe66beee1f1ecbbc09152b0c3aa639a4
root <root@Omar-PC.localdomain>
2024-02-18 16:58:12 +0200

    6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class State(Base):
+	"""
+	Represents a state in the database.
+
+	Attributes:
+		id (int): The unique identifier of the state.
+		name (str): The name of the state.
+	"""
+	__tablename__ = 'states'
+	id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+	name = Column(String(128), nullable=False)
+
+# Connect to MySQL server running on localhost at port 3306
+engine = create_engine('mysql://localhost:3306')
+
+# Import all classes that inherit from Base
+# ...
+
+# Create all tables in the database
+Base.metadata.create_all(engine)
